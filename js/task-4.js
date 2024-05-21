8ea88f612aa074a41809d1ce70b35521d659ecc9
YULIYA2929 <138712567+YULIYA2929@users.noreply.github.com>
2024-05-21 20:07:56 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,8 +1,24 @@
-const form = document.querySelector(".login-form")
-form.addEventListener("submit", handleSubmit)
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
-    event.preventDefault()
-    console.log(event)
+    event.preventDefault();
+    console.log(event);
 
+    const elements = event.currentTarget.elements;
+
+    const info = {
+        email: elements.email.value,
+        password: elements.password.value
+    };
+
+    if(info.email === "" || info.password === "") {
+        alert('All form fields must be filled in') ;
+
+    } else {
+        console.log(info);
+    }
+
+    event.currentTarget.reset();
 }
+
