f8aed52b500abfa23453134c445be7fb9ecf7f1a
YULIYA2929 <138712567+YULIYA2929@users.noreply.github.com>
2024-05-21 19:18:45 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,8 @@
+const form = document.querySelector(".login-form")
+form.addEventListener("submit", handleSubmit)
 
+function handleSubmit(event) {
+    event.preventDefault()
+    console.log(event)
+
+}
